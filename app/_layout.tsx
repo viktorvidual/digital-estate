361c2031ba47d8a2e5ebc2f0a5cda2916945b2df
viksito <viktor.martinov.p@gmail.com>
2025-03-25 13:56:01 +0200

    Adding access check for non auth users

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,18 +4,21 @@ import { tamaguiConfig } from '../tamagui.config';
 import { ToastProvider } from '@tamagui/toast';
 import { Header } from '@/components/Header/Header';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useSegments, router } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
 import { supabase } from '@/lib/supabase';
 import { useAuthStore } from '@/stores';
 
+const UNAUTHORIZED_ROUTES = ['login', 'register'];
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const segments = useSegments();
   const [sessionLoading, setSessionLoading] = useState(true);
-  const { setSession } = useAuthStore();
+  const { setSession, session } = useAuthStore();
 
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -43,6 +46,17 @@ export default function RootLayout() {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
+
+  useEffect(() => {
+    console.log('segments', segments);
+    if (loaded && !sessionLoading) {
+      const isInNonAuthGroup = UNAUTHORIZED_ROUTES.includes(segments[0]);
+
+      if (isInNonAuthGroup && session) {
+        router.replace('/');
+      }
+    }
+  }, [segments, session]);
 
   if (!loaded || sessionLoading) {
     return null;
